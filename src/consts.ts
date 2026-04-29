f708c2b46cbc2e38a69023f5384bdf1fb2acffe4
Mau <alan.fernandez@ucb.edu.bo>
2026-04-29 12:00:11 -0400

    subiendo actualización

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Mau /Dev';
+export const SITE_DESCRIPTION = 'Este es mi portafolio, en donde muestro mis proyectos y tambien todo sobre mi conocimiento en desarrollo Backend, Frontend, Dispositivos Móviles y Videojuegos';
---
+++ src/links/list-links.ts
@@ -0,0 +1,13 @@
+
+// SOCIAL
+export const GITHUB_ACCOUNT=""
+export const EMAIL_ACCOUNT=""
+export const FACEBOOK_ACCOUNT=""
+export const YOUTUBE_ACCOUNT=""
+export const LINKEDIN_ACCOUNT=""
+export const INSTAGRAM_ACCOUNT=""
+export const REDDIT_ACCOUNT=""
+
+// PROJECTS
+
+export const IMG_PROYECTO_TEMPLATE = "https://fastly.picsum.photos/id/0/400/240.jpg?hmac=hcOKHeO-N0915WF7UZeEs78iH7D3wyst2t1xBtDFtE8"
